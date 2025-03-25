c5d2973a4b533c38ee14823a583f4b10ecfe7c21
yehosuahercules <yehosua.hercules4c@gmail.com>
2025-03-25 10:09:18 -0600

    initial commit

---
+++ src/main/java/org/example/LispInterpreter.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+public class LispInterpreter {
+    public static void main(String[] args) {
+        // ITokenizer tokenizer = new LispTokenizer();
+        //LispEvaluator evaluator = new LispEvaluator();
+
+        //String expr1 = "(+ 5 (* 2 3))";
+        //System.out.println("Resultado: " + evaluator.evaluate(tokenizer.tokenize(expr1)));
+
+        //String expr2 = "(+ 3 4 (* 2 3 ) 5 )";
+        //System.out.println("Resultado " + evaluator.evaluate(tokenizer.tokenize(expr2)));
+
+        //String expr3 = "(SETQ X 10)";
+        //System.out.println("Asignación SETQ: " + evaluator.evaluate(tokenizer.tokenize(expr3)));
+
+        //String expr4 = "(+ X 5)";
+        //System.out.println("Uso de variable X: " + evaluator.evaluate(tokenizer.tokenize(expr4)));
+
+    }
+}
